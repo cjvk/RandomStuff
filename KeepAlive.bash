cb66a5043561d0218f17dd7efc0da9926bef9bce
Chris von Krogh <cvonkrogh@paypal.com>
2017-08-10 13:41:47 -0700

    adding i/100 to KeepAlive

--- KeepAlive.bash
+++ KeepAlive.bash
@@ -1,1 +1,1 @@
-for i in {1..100}; do echo -n '['; echo -n `date`; echo -n ']    '; ping -c 1 www.google.com | grep 'packets transmitted'; sleep 300 ; done
+for i in {1..100}; do echo -n "[$i/100]    "; echo -n '['; echo -n `date`; echo -n ']    '; ping -c 1 www.google.com | grep 'packets transmitted'; sleep 300 ; done
